0fd8ae3be31fb83ede6d45776bd4950cff99f79c
abeljosep <abeljosepm@gmail.com>
2022-03-02 16:51:53 -0400

    middleware

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router('./db.json'); // <== Will be created later
+const middlewares = jsonServer.defaults({
+    static: "./build",
+});
 const port = process.env.PORT || 4000; // <== You can change the port
 
 server.use(middlewares);
